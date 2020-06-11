c9cfa22ba181bd4f5dad58dbf83a68165321dd88
jianyun <jianyun.zhuo@outlook.com>
2020-06-11 23:35:48 +0800

    611done

--- app/main/views.py
+++ app/main/views.py
@@ -42,18 +42,12 @@ def index():
         db.session.commit()
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
-    show_followed = False
-    if current_user.is_authenticated:
-        show_followed = bool(request.cookies.get('show_followed', ''))
-    if show_followed:
-        query = current_user.followed_posts
-    else:
-        query = Post.query
-    pagination = query.order_by(Post.timestamp.desc()).paginate(
+    query = Comment.query
+    pagination = query.order_by(Comment.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out=False)
-    posts = pagination.items
-    return render_template('index.html', form=form, posts=posts,
+    comments = pagination.items
+    return render_template('index.html', form=form, comments = comments,
                            show_followed=show_followed, pagination=pagination)
 
 @main.route('/user/<username>')
